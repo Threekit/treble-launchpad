b95ac56ca53e4757ddd8c2751e3380cf020e5080
Amaan Saeed <asaeed@threekit.com>
2021-12-15 10:16:23 -0500

    uses subdomains

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const morgan = require("morgan");
 const path = require("path");
 const { updateCatalogThumbnails } = require("./utils");
 
-// const hostname = "humantouch.dev.3kit.com";
-
 const app = express();
 
 const PORT = 80;
@@ -20,16 +18,15 @@ app.get("/api/health", (req, res) => {
 });
 
 app.use("/", (req, res, next) => {
-  console.log(req.hostname);
-  console.log("handling request");
-  const env = req.hostname.split(".3kit")[0].split(".")[1];
-  express.static(path.join(__dirname, "build", env))(req, res, next);
+  const env = req.subdomains[1] || "dev";
+  req.url = `/${env}${req.originalUrl}`;
+  next();
 });
 
+app.use(express.static(path.join(__dirname, "build")));
+
 app.get("*", (req, res) => {
-  console.log(req.hostname);
-  const env = req.hostname.split(".3kit")[0].split(".")[1];
-  res.sendFile(path.join(__dirname, "build", env, "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => console.log("listening on port: ", PORT));
