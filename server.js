e994fc5824a443f46f903d1491d37756e5fc1d7d
Amaan Saeed <asaeed@threekit.com>
2021-12-15 00:21:28 -0500

    logs hostname

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const path = require("path");
 const { updateCatalogThumbnails } = require("./utils");
 
+// const hostname = "humantouch.dev.3kit.com";
+
 const app = express();
 
 const PORT = 80;
@@ -18,10 +20,12 @@ app.get("/api/health", (req, res) => {
 });
 
 app.use((req, res, next) => {
+  console.log(req.hostname);
   const env = req.hostname.split(".3kit")[0].split(".")[1];
   express.static(path.join(__dirname, "build", env))(req, res, next);
 });
 app.get("*", (req, res) => {
+  console.log(req.hostname);
   const env = req.hostname.split(".3kit")[0].split(".")[1];
   res.sendFile(path.join(__dirname, "build", env, "index.html"));
 });
