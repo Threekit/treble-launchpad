f1caa9ba9e38a897c1f4cddf269c8afe5e2098fa
Amaan Saeed <asaeed@threekit.com>
2021-12-15 11:23:30 -0500

    logs url transform

--- server.js
+++ server.js
@@ -19,9 +19,8 @@ app.get("/api/health", (req, res) => {
 
 app.use("/", (req, res, next) => {
   const env = req.subdomains[0] || "dev";
-  console.log("subdomains", req.subdomains);
-  console.log("env", env);
   req.url = `/${env}${req.originalUrl}`;
+  console.log(`url transform: ${req.originalUrl} -> ${req.url}`);
   next();
 });
 
